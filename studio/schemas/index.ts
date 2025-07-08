4b32a66b31c40323a2ed3992f4cc1945f2edb9ae
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-07-08 12:47:21 -0400

    created menu page schema

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,6 +1,7 @@
 import blockContent from './blockContent'
 import home from './home'
 import navigation from './navigation'
+import menu from './menu'
 import footer from './footer'
 
-export const schemaTypes = [blockContent, navigation, home, footer]
+export const schemaTypes = [blockContent, navigation, home, menu, footer]
---
+++ studio/schemas/menu.ts
@@ -0,0 +1,46 @@
+import {defineType} from 'sanity'
+
+export default defineType({
+  name: 'menuPage',
+  title: 'Menu Page',
+  type: 'document',
+  fields: [
+    // {
+    //   name: 'title',
+    //   title: 'Section Title',
+    //   type: 'string',
+    //   validation: (Rule) => Rule.required(),
+    // },
+    {
+      name: 'images',
+      title: 'Section Images',
+      type: 'array',
+      of: [
+        {
+          type: 'image',
+          options: {hotspot: true},
+          fields: [
+            {
+              name: 'name',
+              title: 'Menu Image Name',
+              type: 'string',
+              validation: (Rule) => Rule.required(),
+            },
+            {
+              name: 'uploadedAt',
+              title: 'Date Uploaded',
+              type: 'datetime',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  preview: {
+    prepare() {
+      return {
+        title: 'Menu Page',
+      }
+    },
+  },
+})
