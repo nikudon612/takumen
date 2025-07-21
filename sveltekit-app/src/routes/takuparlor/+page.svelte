4e82b45fd6f1c127b30d6d133cc6e9d8613e9a67
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-07-21 19:46:03 -0400

    updated takuparlor page layout, menu is scrollable while images stay absolute

--- sveltekit-app/src/routes/takuparlor/+page.svelte
+++ sveltekit-app/src/routes/takuparlor/+page.svelte
@@ -10,25 +10,35 @@
 	<div class="menu-left">
 		<img src={MascotTop} alt="Mascot" class="mascot-top" />
 
-		<div class="menu-items">
-			<h2>SOFTSERVE</h2>
-			<p>matcha<br />vanilla<br />swirl</p>
+		<div class="menu-scroll-wrapper">
+			<div class="menu-items">
+				<h2>SOFTSERVE</h2>
+				<p>matcha<br />vanilla<br />swirl</p>
 
-			<h2>CREAM ANMITSU</h2>
-			<p>
-				<strong>hojicha</strong><br />hojicha kanten, original jello, shiratama mochi, redbean
-				paste, matcha soft serve, kinako, homemade maple sauce
-			</p>
-			<p>
-				<strong>mango</strong><br />mango kanten, original jello, shiratama mochi, redbean paste,
-				mango pulp, vanilla soft serve, homemade mango sauce
-			</p>
+				<h2>CREAM ANMITSU</h2>
+				<p>
+					<strong>hojicha</strong><br />hojicha kanten, original jello, shiratama mochi, redbean
+					paste, matcha soft serve, kinako, homemade maple sauce
+				</p>
+				<p>
+					<strong>mango</strong><br />mango kanten, original jello, shiratama mochi, redbean paste,
+					mango pulp, vanilla soft serve, homemade mango sauce
+				</p>
 
-			<h2>ICE CREAM FLOAT</h2>
-			<p>melon soda & vanilla<br />matcha & matcha<br />coffee & vanilla</p>
+				<h2>ICE CREAM FLOAT</h2>
+				<p>melon soda & vanilla<br />matcha & matcha<br />coffee & vanilla</p>
 
-			<h2>MOCHIDOKI</h2>
-			<p>mochi ice cream<br />assorted flavors</p>
+				<h2>MOCHIDOKI</h2>
+				<p>mochi ice cream<br />assorted flavors</p>
+			</div>
+			<h2>TEST SECTION</h2>
+			<p>line 1<br />line 2<br />line 3<br />line 4<br />line 5</p>
+			<h2>TEST SECTION</h2>
+			<p>line 1<br />line 2<br />line 3<br />line 4<br />line 5</p>
+			<h2>TEST SECTION</h2>
+			<p>line 1<br />line 2<br />line 3<br />line 4<br />line 5</p>
+			<h2>TEST SECTION</h2>
+			<p>line 1<br />line 2<br />line 3<br />line 4<br />line 5</p>
 		</div>
 
 		<img src={MascotBottom} alt="Mascot cheering" class="mascot-bottom" />
@@ -42,19 +52,33 @@
 <style>
 	.split-menu {
 		display: flex;
-		flex-direction: row;
-		min-height: 100vh;
+		width: 100vw;
+		height: 100vh; /* full viewport height */
 	}
 
 	.menu-left {
 		position: relative;
 		flex: 1;
-		padding: 4rem 2rem;
 		background-color: #789faf;
 		color: white;
 		font-family: avenir-next-lt-pro-condensed, sans-serif;
-		overflow: visible; /* ensure nothing clips it */
-		/* needed for absolute positioning children */
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.menu-scroll-wrapper {
+		flex: 1;
+		overflow-y: auto;
+		padding: 4rem 2rem;
+
+		/* Hide scrollbar for WebKit (Chrome, Safari) */
+		scrollbar-width: none; /* Firefox */
+		-ms-overflow-style: none; /* IE 10+ */
+	}
+
+	.menu-scroll-wrapper::-webkit-scrollbar {
+		display: none; /* WebKit */
 	}
 
 	.menu-items {
@@ -70,25 +94,25 @@
 	}
 
 	.menu-items p {
-		font-size: 1rem;
+		font-size: 1.125rem;
 		line-height: 1.5;
 		font-family: Futura, sans-serif;
 	}
 
 	.mascot-top,
 	.mascot-bottom {
-		width: 80px;
+		position: absolute;
+		z-index: 10;
+		pointer-events: none;
 	}
 
 	.mascot-top {
-		position: absolute;
 		top: 2rem;
 		right: 6rem;
 		width: 120px;
 	}
 
 	.mascot-bottom {
-		position: absolute;
 		bottom: 16rem;
 		left: 5rem;
 		width: 150px;
@@ -96,6 +120,8 @@
 
 	.menu-right {
 		flex: 1;
+		height: 100vh;
+		overflow: hidden;
 	}
 
 	.menu-right img {
