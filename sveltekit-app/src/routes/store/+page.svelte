bf0b49736506147adf21b1f3429b7213cbb9de2b
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-07-22 18:34:37 -0400

    componentized orderonline

--- sveltekit-app/src/routes/store/+page.svelte
+++ sveltekit-app/src/routes/store/+page.svelte
@@ -1,11 +1,36 @@
-<script></script>
+<script>
+	import image from '../../lib/assets/mascot2.png';
+</script>
 
-<section class="store-section"></section>
+<section class="store-section">
+	<div class="content">
+		<h1 class="title">Coming Soon!</h1>
+		<img src={image} alt="Store Coming Soon" />
+	</div>
+</section>
 
 <style>
 	.store-section {
-		padding: 2rem;
+		padding: 3rem;
 		background-color: #87b28b;
 		height: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.content {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		color: white;
+		gap: 1rem;
+	}
+
+	.title {
+		font-family: avenir-next-lt-pro-condensed, sans-serif;
+		color: white;
+		text-align: center;
+		font-size: 2.5rem;
+		margin: 0;
 	}
 </style>
