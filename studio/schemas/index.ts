d78450be4af007447718c7654ab14a1b7fbf5e73
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2025-11-20 20:13:06 -0500

    updated takuparlor schema title

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -4,6 +4,6 @@ import navigation from './navigation'
 import menu from './menu'
 import footer from './footer'
 import exhibition from './exhibition'
-import takuparlorPage from './takuparlorPage'
+import takuParlorPage from './takuParlorPage'
 
-export const schemaTypes = [blockContent, navigation, home, menu, footer, exhibition, takuparlorPage]
+export const schemaTypes = [blockContent, navigation, home, menu, footer, exhibition, takuParlorPage]
